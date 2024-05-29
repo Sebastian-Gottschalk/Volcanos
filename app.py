055476a8c69f9ff91bf794646b6c47f4310c0ea0
Farius97 <sebastian.gottschalk@gmx.net>
2024-05-29 18:01:37 +0200

    added functions

--- app.py
+++ app.py
@@ -7,25 +7,38 @@ import plotly.io as pio
 
 volcano_df = pd.read_csv('./data/volcano_ds_pop.csv')
 volcano_json = json.load(open('./data/countries.geojson'))
-#st.dataframe(volcano_df)
 
-def plot_nr_of_volcanos_by_status(df, status, volcano_json):
-    fig = px.choropleth_mapbox(
-        df,
-        geojson = volcano_json,
-        featureidkey = 'properties.ISO_A3',
-        locations = 'ISO', 
-        color = status,
-        color_continuous_scale="Viridis",
-        mapbox_style="carto-positron",
-        zoom=1, 
-        center = {"lat": 40 , "lon": 40},
-        opacity=0.5,
-        #hover_data = {'Country' : True, 'ISO': False, status: True},
-        hover_data = {'ISO': False, status: True},
-        hover_name = 'Country'
-    )
-    
+def plot_nr_of_volcanos_by_status(df, status, volcano_json, colorscheme, scale):
+    if scale == 'cont':
+        fig = px.choropleth_mapbox(
+            df,
+            geojson = volcano_json,
+            featureidkey = 'properties.ISO_A3',
+            locations = 'ISO', 
+            color = status,
+            color_continuous_scale=colorscheme,
+            mapbox_style="carto-positron",
+            zoom=1, 
+            center = {"lat": 40 , "lon": 40},
+            opacity=0.5,
+            hover_data = {'ISO': False, status: True},
+            hover_name = 'Country'
+        )
+    else:
+        fig = px.choropleth_mapbox(
+            df,
+            geojson = volcano_json,
+            featureidkey = 'properties.ISO_A3',
+            locations = 'ISO', 
+            color = status,
+            color_discrete_map={'Above Threshhold' : 'green', 'Below Threshhold': 'red'},
+            mapbox_style="carto-positron",
+            zoom=1, 
+            center = {"lat": 40 , "lon": 40},
+            opacity=0.5,
+            hover_data = {'ISO': False, status: True},
+            hover_name = 'Country'
+        )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     
     return fig
@@ -54,9 +67,9 @@ for key in ISO_dict:
 nr_of_volcanos['Population'] = nr_of_volcanos['Number']
 for i in range(len(nr_of_volcanos)):
     if nr_of_volcanos['Population'].iloc[i] == 0:
-        nr_of_volcanos['Population'].iloc[i] = 1
+        nr_of_volcanos.loc[i,'Population'] = 1
     else:
-        nr_of_volcanos['Population'].iloc[i] = volcano_df[volcano_df['ISO'] == nr_of_volcanos['ISO'].iloc[i]]['Population (2020)'].mean()
+        nr_of_volcanos.loc[i,'Population'] = volcano_df[volcano_df['ISO'] == nr_of_volcanos['ISO'].iloc[i]]['Population (2020)'].mean()
 nr_of_volcanos['Volcanos per Population (per Million)'] = nr_of_volcanos['Number'] / nr_of_volcanos['Population']*1000000
 
 for status in volcano_df.Status.unique():
@@ -67,7 +80,6 @@ for status in volcano_df.Status.unique():
     nr_of_volcanos[str(status)] = nr_of_volcanos[str(status)].astype(int)
     nr_of_volcanos[str(status)+' pC'] = nr_of_volcanos[str(status)]/nr_of_volcanos['Population']*1000000
 nr_of_volcanos.rename(columns = {'Number' : 'Total number of volcanos'}, inplace = True)
-#st.dataframe(nr_of_volcanos)
 
 customdata = list(nr_of_volcanos.columns)
 customdata.remove('ISO')
@@ -103,6 +115,11 @@ left_column,right_column = st.columns(2)
 status_list = ['All'] + customdata[2:]
 status = left_column.selectbox('Select the status',status_list)
 
+color_list = ['Mark everything above a threshhold']+px.colors.named_colorscales()
+
+threshhold_list = []
+for i in range(1000):
+    threshhold_list.append(i/1000)
 
 show_all_volcanos = right_column.checkbox('Show Volcanos individually')
 
@@ -112,6 +129,7 @@ if show_all_volcanos:
     else:
         st.map(volcano_df[volcano_df['Status'] == status])
 else:
+    colorscheme = left_column.selectbox('Select the color scheme',color_list)  
     data_shown = right_column.checkbox('Show Volcanos per million capita')
     if data_shown:
         display_value = 'Volcanos per Population (per Million)'
@@ -119,22 +137,43 @@ else:
         display_value = 'Total number of volcanos'
         
     if status == 'All':
-        fig1 = px.choropleth_mapbox(
-            nr_of_volcanos,
-            geojson = volcano_json,
-            featureidkey = 'properties.ISO_A3',
-            custom_data = customdata,
-            locations = 'ISO', 
-            #color = 'Total number of volcanos',
-            color = display_value,
-            color_continuous_scale="Viridis",
-            mapbox_style="carto-positron",
-            zoom=1, 
-            center = {"lat": 40 , "lon": 40},
-            opacity=0.5,
-            hover_data = hover_dict,
-            hover_name = 'Country',
-        )
+        if colorscheme == 'Mark everything above a threshhold':
+            threshhold = right_column.select_slider("Select a threshhold (the countries with a proportion to the maximum higher than the threshhold will be displayed)", threshhold_list)
+            max_volcanos = max(nr_of_volcanos[display_value])
+            nr_of_volcanos= nr_of_volcanos.assign(Threshhold = lambda row:row[display_value]/max_volcanos> threshhold)
+            nr_of_volcanos['Threshhold'] = nr_of_volcanos['Threshhold'].map({True: 'Above Threshhold', False : 'Below Threshhold'})
+            fig1 = px.choropleth_mapbox(
+                nr_of_volcanos,
+                geojson = volcano_json,
+                featureidkey = 'properties.ISO_A3',
+                custom_data = customdata,
+                locations = 'ISO',
+                color = 'Threshhold',
+                color_discrete_map={'Above Threshhold' : 'green', 'Below Threshhold': 'red'},
+                mapbox_style="carto-positron",
+                zoom=1, 
+                center = {"lat": 40 , "lon": 40},
+                opacity=0.5,
+                hover_data = hover_dict,
+                hover_name = 'Country',
+            )
+        else:
+            fig1 = px.choropleth_mapbox(
+                nr_of_volcanos,
+                geojson = volcano_json,
+                featureidkey = 'properties.ISO_A3',
+                custom_data = customdata,
+                locations = 'ISO', 
+                color = display_value,
+                color_continuous_scale=colorscheme,
+                mapbox_style="carto-positron",
+                zoom=1, 
+                center = {"lat": 40 , "lon": 40},
+                opacity=0.5,
+                hover_data = hover_dict,
+                hover_name = 'Country',
+            )
+
 
         fig1.update_layout(
             title = {'text' : 'Test', 'font' : {'size' : 20}},
@@ -142,9 +181,25 @@ else:
         )
 
     else:
+        scale = 'cont'
         if data_shown:
-            fig1 = plot_nr_of_volcanos_by_status(nr_of_volcanos, status+' pC', volcano_json)
+            status = status + ' pC'
+            if colorscheme == 'Mark everything above a threshhold':
+                scale = 'disc'
+                threshhold = right_column.select_slider("Select a threshhold (the countries with a proportion to the maximum higher than the threshhold will be displayed)", threshhold_list)
+                max_volcanos = max(nr_of_volcanos[str(status)])
+                nr_of_volcanos= nr_of_volcanos.assign(Threshhold = lambda row:row[str(status)]/max_volcanos> threshhold)
+                nr_of_volcanos['Threshhold'] = nr_of_volcanos['Threshhold'].map({True: 'Above Threshhold', False : 'Below Threshhold'})
+                status = 'Threshhold'
+            fig1 = plot_nr_of_volcanos_by_status(nr_of_volcanos, status, volcano_json, colorscheme,scale)
         else:
-            fig1 = plot_nr_of_volcanos_by_status(nr_of_volcanos, status, volcano_json)
+            if colorscheme == 'Mark everything above a threshhold':
+                scale = 'disc'
+                threshhold = right_column.select_slider("Select a threshhold (the countries with a proportion to the maximum higher than the threshhold will be displayed)", threshhold_list)
+                max_volcanos = max(nr_of_volcanos[str(status)])
+                nr_of_volcanos= nr_of_volcanos.assign(Threshhold = lambda row:row[str(status)]/max_volcanos> threshhold)
+                nr_of_volcanos['Threshhold'] = nr_of_volcanos['Threshhold'].map({True: 'Above Threshhold', False : 'Below Threshhold'})
+                status = 'Threshhold'
+            fig1 = plot_nr_of_volcanos_by_status(nr_of_volcanos, status, volcano_json, colorscheme,scale)
 
     st.plotly_chart(fig1)
